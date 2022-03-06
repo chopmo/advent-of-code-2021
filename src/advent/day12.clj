e904a3add1dd9a678b376e4a7f5e33b96affeff4
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2022-03-06 19:39:52 +0100

    Refactored
    Slower but more flexible

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -15,17 +15,37 @@
 (defn small-cave? [node]
   (= node (str/lower-case node)))
 
-(defn visited-small-cave? [path node]
-  (and (small-cave? node)
-       (get (set path) node)))
+(defn valid-path? [allow-one-double-visit? path]
+  (let [pruned-path      (filter small-cave? path)
+        freqs            (frequencies pruned-path)
+        small-cave-freqs (dissoc freqs "start" "end")]
+    (and
+     ;; start and end visited at most once
+     (<= (get freqs "start" 0) 1)
+     (<= (get freqs "end" 0) 1)
+
+     ;; No small cave visited more than twice
+     (empty? (filter (fn [[_ v]] (> v 2)) small-cave-freqs))
+
+     ;; Maybe allow at most 1 small cave visited twice
+     (if allow-one-double-visit?
+       (<= (->> small-cave-freqs
+                (filter (fn [[_ v]] (= v 2)))
+                count)
+           1)
+       (zero? (->> small-cave-freqs
+                   (filter (fn [[_ v]] (> v 1)))
+                   count))))))
 
 (defn find-paths [connections path]
   (let [last-node (last path)]
     (if (= last-node "end")
       [path]
-      (mapcat #(find-paths connections (concat path [%]))
-              (remove (partial visited-small-cave? path)
-                      (connections-from connections last-node))))))
+      (let [possible-paths (map #(concat path [%])
+                                (connections-from connections last-node))]
+        (mapcat #(find-paths connections %)
+                (filter (partial valid-path? false) possible-paths))))))
+
 (defn puzzle12-1 []
   (count
    (find-paths (read-connections "resources/input12.txt")
