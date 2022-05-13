6eabf12be8414f870b11f547d011617e705064d2
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2022-05-13 17:21:41 +0200

    wip

--- src/advent/day19.clj
+++ src/advent/day19.clj
@@ -23,17 +23,17 @@
 
 ;; Z points inwards
 ;; Positive angles mean CCW rotation
-(defn rot-z [[x y z] r]
+(defn rot-z [r [x y z]]
   [(math/round (- (* x (math/cos r)) (* y (math/sin r))))
    (math/round (+ (* x (math/sin r)) (* y (math/cos r))))
    z])
 
-(defn rot-y [[x y z] r]
+(defn rot-y [r [x y z]]
   [(math/round (+ (* x (math/cos r)) (* z (math/sin r))))
    y
    (math/round (+ (* -1 x (math/sin r)) (* z (math/cos r))))])
 
-(defn rot-x [[x y z] r]
+(defn rot-x [r [x y z]]
   [x
    (math/round (- (* y (math/cos r)) (* z (math/sin r))))
    (math/round (+ (* y (math/sin r)) (* z (math/cos r))))])
@@ -41,21 +41,44 @@
 
 (defn z-orientations [p]
   (for [i (range 4)]
-    (rot-z p (* i (/ math/PI 2)))))
+    (rot-z (* i (/ math/PI 2)) p)))
 
 (defn transformations [p]
   (let [pi-halves  (/ math/PI 2)
         viewpoints [p                          ;; Look into z
-                    (rot-y p pi-halves)        ;; Look left on x
-                    (rot-y p (* -1 pi-halves)) ;; Look right on x
-                    (rot-y p (* 2 pi-halves))  ;; Look out of z
-                    (rot-x p pi-halves)        ;; Look up on Y
-                    (rot-x p (* -1 pi-halves)) ;; Look down on Y
+                    (rot-y pi-halves p)        ;; Look left on x
+                    (rot-y (* -1 pi-halves) p) ;; Look right on x
+                    (rot-y (* 2 pi-halves) p)  ;; Look out of z
+                    (rot-x pi-halves p)        ;; Look up on Y
+                    (rot-x (* -1 pi-halves) p) ;; Look down on Y
                     ]]
     ;;(mapcat z-orientations viewpoints)
     viewpoints
     ))
+
+(def z-orientation-fns
+  (for [i (range 4)]
+    (partial rot-z (* i (/ math/PI 2)))))
+
+(def viewpoint-fns
+  (let [pi-halves (/ math/PI 2)]
+    [identity                      ;; Look into z
+     (partial rot-y pi-halves)     ;; Look left on x
+     (partial rot-y (* -1 pi-halves)) ;; Look right on x
+     (partial rot-y (* 2 pi-halves))  ;; Look out of z
+     (partial rot-x pi-halves)        ;; Look up on Y
+     (partial rot-x (* -1 pi-halves)) ;; Look down on Y
+     ]))
+
+(def transformation-fns
+  (for [viewpoint-fn     viewpoint-fns
+        z-orientation-fn z-orientation-fns]
+    (comp z-orientation-fn viewpoint-fn)))
+
+
+
 (comment
+  ((second transformation-fns) [1 1 1])
   (transformations [1 2 3])
   (z-orientations [1 1 1])
   (rot-z [1 1 0] (/ math/PI 2))
