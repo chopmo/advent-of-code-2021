e6f379a2edb13cb01be3153bdbfa5f4c1a0ccf93
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2022-06-05 15:29:43 +0200

    day 19.1 finally complete (and slow)

--- src/advent/day19.clj
+++ src/advent/day19.clj
@@ -8,12 +8,13 @@
   (map #(Integer/parseInt %) (str/split s #",")))
 
 (defn read-input [filename]
+  ;; This is so ugly.
   (->> (slurp filename)
        (str/split-lines)
-       (filter seq)
-       (filter (complement (partial re-matches #"---.*")))
-       (map parse-coordinate)
-       (partition 25)))
+       (filter #(not (re-matches #"---.*" %)))
+       (partition-by str/blank?)
+       (filter #(> (count %) 1))
+       (map #(map parse-coordinate %))))
 
 (defn dist [[x1 y1 z1] [x2 y2 z2]]
   [(- x2 x1)
@@ -37,8 +38,6 @@
    (math/round (- (* y (math/cos r)) (* z (math/sin r))))
    (math/round (+ (* y (math/sin r)) (* z (math/cos r))))])
 
-
-
 (def z-orientation-fns
   (for [i (range 4)]
     (partial rot-z (* i (/ math/PI 2)))))
@@ -58,88 +57,26 @@
         z-orientation-fn z-orientation-fns]
     (comp z-orientation-fn viewpoint-fn)))
 
-
-(defn puzzle19-1 []
-  (let [scanners (read-input "resources/day19-example.txt")
-        s0 (first scanners)]
-    (map (second transformation-fns) (first scanners))
-    ))
-
-(deftest dist-test
-  (is (= [1 2 3] (dist [0 0 0] [1 2 3])))
-  (is (= [1 -8 2] (dist [-1 5 7] [0 -3 9]))))
-
-
-(comment
-  (let [scanners (read-input "resources/day19-all-matching.txt")
-        s0       (first scanners)
-        s1       (second scanners)]
-
-    #_(for [f transformation-fns]
-      (sort (map dist s0 (map f s1))))
-
-    s0
-    )
-  ,)
-
-
-(defn scanner-dist [s1 s2]
-  (let [beacon-dists (map dist s1 s2)
-        freqs (frequencies beacon-dists)]
-    (->> freqs
-         (filter #(>= (second %) 12))
-         first
-         first)
-    ))
-
-(comment
-  (let [match0 '((-618,-824,-621)
-                (-537,-823,-458)
-                (-447,-329,318)
-                (404,-588,-901)
-                (544,-627,-890)
-                (528,-643,409)
-                (-661,-816,-575)
-                (390,-675,-793)
-                (423,-701,434)
-                (-345,-311,381)
-                (459,-707,401)
-                (-485,-357,347))
-
-        match1 '((686,422,578)
-                (605,423,415)
-                (515,917,-361)
-                (-336,658,858)
-                (-476,619,847)
-                (-460,603,-452)
-                (729,430,532)
-                (-322,571,750)
-                (-355,545,-477)
-                (413,935,-424)
-                (-391,539,-444)
-                (553,889,-390))
-
-        ]
-    (for [f transformation-fns]
-                (scanner-dist (map f match1) match0))
-
-    )
-  ,)
-
-
 (defn find-distance [beacons1 beacons2]
   (let [beacon-dists (for [b1 beacons1 b2 beacons2] (dist b1 b2))
         freqs        (frequencies beacon-dists)]
     (->> freqs
          (filter #(>= (second %) 12))
          first
-         first)
-    ))
+         first)))
+
+(defn all-distances [bs1 bs2]
+  (for [b1 bs1 b2 bs2]
+    [(dist b1 b2) b1 b2]))
 
 (defn find-matching-transformation-fn [world beacons]
   (->> transformation-fns
        (filter (fn [t-fn] (some? (find-distance world (map t-fn beacons)))))
        first))
+
+(defn transform-and-find-distance [bs1 bs2]
+  (when-let [t-fn (find-matching-transformation-fn bs1 bs2)]
+    (find-distance bs1 (map t-fn bs2))))
 
 (defn overlapping? [world beacons]
   (some? (find-matching-transformation-fn world beacons)))
@@ -152,31 +89,27 @@
         reoriented-beacons (map transformation-fn beacons)
         distance (find-distance reoriented-beacons world)
         translated-beacons (map (partial translate distance) reoriented-beacons)]
-    (clojure.set/union world (set translated-beacons)))
-  )
+    (clojure.set/union world (set translated-beacons))))
+
+(defn merge-scanners [world scanner-q]
+  (if (empty? scanner-q)
+    (count world)
+    (let [scanner (peek scanner-q)]
+      (if (overlapping? world scanner)
+        (merge-scanners (merge-beacons world scanner) (pop scanner-q))
+        (merge-scanners world (conj (pop scanner-q) scanner))))))
 
 
-(comment
-  (let [q (clojure.lang.PersistentQueue/EMPTY)]
-    (conj q 123))
+;;; ********* TESTS *********
 
-  (def q (clojure.lang.PersistentQueue/EMPTY))
-  (def q (conj q 4 5))
-  (seq (pop q))
-
-  ;; conj
-  ;; peek
-  ;; pop
-  ,)
-
+(deftest dist-test
+  (is (= [1 2 3] (dist [0 0 0] [1 2 3])))
+  (is (= [1 -8 2] (dist [-1 5 7] [0 -3 9]))))
 
 (deftest overlapping?-test
   (let [scanners (read-input "resources/day19-example.txt")]
     (is (true? (overlapping? (nth scanners 0)
-                             (nth scanners 1))))
-
-    )
-  )
+                             (nth scanners 1))))))
 
 (deftest find-distance-test
   (let [scanners (read-input "resources/day19-example.txt")
@@ -184,49 +117,11 @@
         scanner1 (nth scanners 1)
         t-fn (find-matching-transformation-fn world scanner1)]
     (is (= [68 -1246 -43]
-           (find-distance (map t-fn scanner1) world)))
+           (find-distance (map t-fn scanner1) world)))))
 
-    )
-  )
-
-(deftest merge-beacons-test
-  (let [scanners (read-input "resources/day19-example.txt")
-        world (set (nth scanners 0))]
-    (-> world
-        (merge-beacons (nth scanners 1))
-        ;(merge-beacons (nth scanners 4))
-        ;(merge-beacons (nth scanners 2))
-        ;(merge-beacons (nth scanners 3))
-        )
-    (overlapping? (nth scanners 1)
-                  (nth scanners 4))
-
-    )
-
-  )
-
-(comment
-  (let [scanners (read-input "resources/day19-example.txt")]
-    (overlapping? (nth scanners 0)
-                  (map (nth transformation-fns 3)  (nth scanners 1))))
-  ,)
-
-
-(comment
-  (let [scanners (read-input "resources/day19-example.txt")]
-    (overlapping? (nth scanners 1)
-                  (nth scanners 4)))
-  ,)
-
-#_([-68 1246 43]
- [-68 1246 43]
- [-68 1246 43]
- [-68 1246 43]
- [-68 1246 43]
- [-68 1246 43]
- [-68 1246 43]
- [-68 1246 43]
- [-68 1246 43]
- [-68 1246 43]
- [-68 1246 43]
- [-68 1246 43])
+(defn puzzle19-1 []
+  (let [scanners (read-input "resources/day19.txt")]
+    (merge-scanners (set (first scanners))
+                    (apply conj
+                           (clojure.lang.PersistentQueue/EMPTY)
+                           (rest scanners)))))
